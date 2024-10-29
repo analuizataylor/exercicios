e9f85ec325c23227396c91c064f69a24b762aeb8
Ana Luiza Menelli Taylor <analuizamenelli@gmail.com>
2024-10-29 16:42:10 -0300

    Exercício 1

---
+++ exercicio1/src/exercicio7/FilmeMain.java
@@ -0,0 +1,15 @@
+package exercicio7;
+
+/*
+1) Faça um programa para cadastrar dados de uma locadora de filmes, onde cada registro será composto pelos
+seguintes campos: código do filme, título do filme e gênero (A – ação, T – terror, D – drama). O programa
+deverá conter as seguintes operações:
+
+a) Incluir filmes no arquivo (vetor).
+b) Consultar o total de filmes de um determinado gênero.
+c) Excluir um filme do arquivo procurando pelo nome
+
+*/
+
+import java.util.Scanner;
+
---
+++ exercicio1/src/exercicio7/FilmeVetor.java
@@ -0,0 +1,72 @@
+package exercicio7;
+
+/*
+- declaração de atributos do vetor
+- construtores do vetor
+- get e set do vetor
+- métodos do vetor
+ */
+
+
+ public class FilmeVetor {
+
+    //atributos privados quando está em um vetor
+    private Filme[] vetor;
+    private int quantidadeVetor;
+
+    //construtores
+    public FilmeVetor (int tamanho) {
+        this.vetor = new Filme[tamanho];
+        this.quantidadeVetor = 0;
+    }
+
+    //getters
+    //publicos + atributo(primeiro) + return
+
+    public Filme getVetor (int indice) {
+        return this.vetor[indice];
+    }
+
+    public int getQuantidadeVetor() {
+        return quantidadeVetor;
+    }
+
+    //setters
+    //publicos + void + 
+    public void setVetor (int indice, Filme novo) {
+        this.vetor[indice] = novo;
+    }
+
+    public void setQuantidadeVetor (int novo) {
+        this.quantidadeVetor = novo;
+    }
+
+
+    //incluir filmes no arquivo
+    public boolean incluirFilme (Filme novo) {
+        if (this.quantidadeVetor == this.vetor.length) {
+            //condição para saber se o arquivo está cheio
+            return false;
+        } else {
+            this.vetor[this.getQuantidadeVetor()] = novo;
+            this.quantidadeVetor ++;
+            return true;
+        }
+    }
+
+    //procurar filmes pelo genero
+    public int procuraGenero(char g, int index){
+        int valor = 0;
+        if(this.filme.length != 0){
+            for(int i = 0; i < index; i++){
+            if(this.filme[i].getGenero() == g){
+                valor++;
+                }
+            }
+            return valor;
+        }else{
+            return valor;
+        }        
+    }
+}
+ }
