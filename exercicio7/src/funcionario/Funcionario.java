b05c0bccab4c186573498973967619c02b4f84a7
Ana Luiza Menelli Taylor <analuizamenelli@gmail.com>
2024-10-28 21:52:30 -0300

    Exercício 7

---
+++ exercicio7/src/funcionario/Funcionario.java
@@ -0,0 +1,61 @@
+
+public class Funcionario {
+
+    // decLaracao de atributos
+    private String nome;
+    private int horasTrabalhadas;
+    private char categoria;
+
+    
+    //contrutores
+    public Funcionario (String n, int h, char c, double s) {
+        this.nome = n;
+        this.horasTrabalhadas = h;
+        this.categoria = c;
+    }
+
+
+    //getters = leem o valor do atributo
+    public String getNome() {
+        return this.nome;
+    }
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+    public char getCategoria() {
+        return categoria;
+    }
+
+
+    //setters = alterar valor do atributo
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+    public void setCategoria(char categoria) {
+        this.categoria = categoria;
+    }
+
+    public double calcularSalario() {
+
+        double salarioBruto = horasTrabalhadas * 112;
+
+        if (this.categoria == 'G') {
+            salarioBruto += salarioBruto * 1.15;
+        } else if (this.categoria == 'O') {
+            salarioBruto += salarioBruto * 1.10;
+        }
+        return salarioBruto;
+    } 
+
+
+    //toString = mostra os conteúdos dos atributos
+    public String toString() {
+        return "Nome: " + this.nome + "\n" +
+            "Horas trabalhadas: " + this.horasTrabalhadas + "\n" +
+            "Categoria: " + this.categoria + "\n" +
+            "Salario Bruto: " + calcularSalario() + "\n";
+    }
+}
---
+++ exercicio7/src/funcionario/FuncionarioMain.java
@@ -0,0 +1,56 @@
+package funcionario;
+/*
+7) Uma empresa possui um cadastro de funcionários com os seguintes dados: nome, número de horas
+trabalhadas no mês e categoria (pode ser O - Operário ou G-gerente). Sabendo-se que essa empresa deseja
+informatizar sua folha de pagamento. Faça um programa que:
+
+a) Cadastre os dados dos funcionários. 
+O máximo que a empresa suporta são 18 funcionários ativos;
+b) Alterar o número de horas trabalhadas de um determinado funcionário;
+c) Mostrar todos os dados dos funcionários e seus respectivos salários brutos. Para calcular o salário bruto
+adote o valor de R$112,00 para uma hora trabalha + acréscimo da categoria do funcionário. Ver tabela
+abaixo:
+G - 15%
+O - 10%
+ */
+
+
+import java.util.Scanner;
+
+public class FuncionarioMain {
+
+    // scanner declarado como global
+    Scanner entrada = new Scanner(System.in);
+
+    public static void main (String[] args) {
+
+        // cria variavel de opcao do menu
+        char opcao;
+
+        //instancia o vetor
+        FuncionarioVetor empresa = new FuncionarioVetor(18);
+        do {
+            System.out.println("1. Cadastrar um novo funcionário \n"+
+            "2. Alterar horas trabalhadas \n"+
+            "3. Mostrar dados dos funcionários \n" +
+            "4. Sair do menu");
+
+            opcao = scan.next().charAt(0);
+            switch (opcao) {
+            
+            case '1':
+                //metodo
+                break;
+            
+            }
+        } while (opcao != 4);
+
+        entrada.close();
+    }
+
+    
+
+    
+
+
+}
